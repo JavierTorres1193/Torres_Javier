06ec74b12bbd831c88f0a16b4c2b5f5e2361febb
JavierTorres1193 <113069095+JavierTorres1193@users.noreply.github.com>
2022-09-29 16:09:29 -0300

    Update and rename ejercicios.js to ejercicios 1.js

---
+++ modulo01/TP1/ejercicios 1.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Ejercicio 1 </title>
+</head>
+<body>
+<script>
+
+ //Va mostrar en la página web la oración "¡Hola Mundo!"
+ document.writeln("Hola Mundo!")
+ document.writeln ("</br>"); //Realiza un espaciado
+ //Va mostrar en la página web la oración "Soy el primer script"
+ document.writeln("Soy el primer script")
+
+</script>
+
+</body>
+
+
+</html>
