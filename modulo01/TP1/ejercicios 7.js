b69f15370b6842f0f248c41db2c2a8fc116eec29
JavierTorres1193 <113069095+JavierTorres1193@users.noreply.github.com>
2022-09-29 16:12:07 -0300

    Create ejercicios 7.js

---
+++ modulo01/TP1/ejercicios 7.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+<script>
+let dato, resultado;
+val1 = window.prompt("Introduce tu nombre", "..."); //Con un aviso, da la opción de ingresar por teclado tu nombre
+val2 = window.prompt("Introduce tu apellido", "..."); // Con un aviso, da la opción de ingresar por teclado tu apellido
+resultado = `Concatenado tu nombre y apellido es: ${val1} ${val2} ` ; // Concatena las dos respuestas 
+document.write(resultado); // Muestra el resultado 
+</script>
+</body>
+</html>
