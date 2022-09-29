0329b044663e58dea45ecdc43eeb3079cebdb259
JavierTorres1193 <113069095+JavierTorres1193@users.noreply.github.com>
2022-09-29 16:13:31 -0300

    Create ejercicios 9.js

---
+++ modulo01/TP1/ejercicios 9.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<h1><b> Vocales</h1></b>
+</head>
+<body>
+<script>
+var vocales = ["A </br>", "E </br>", "I <br>", "O <br>", "U <br>"];
+for(var i = 1; i<= vocales.length;i++){
+}
+console.log (vocales);
+document.writeln (vocales);
+
+</script>
+
+</body>
