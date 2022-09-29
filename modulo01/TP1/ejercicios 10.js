bc00476ef24e9ddceecdcfc6ec4ce8c5ed22bdb5
JavierTorres1193 <113069095+JavierTorres1193@users.noreply.github.com>
2022-09-29 16:14:03 -0300

    Create ejercicios 10.js

---
+++ modulo01/TP1/ejercicios 10.js
@@ -0,0 +1,15 @@
+!DOCTYPE html>
+<html>
+<head>
+<h1><b> Meses</h1></b>
+</head>
+<body>
+<script>
+var Meses = ["Enero" , "Febrero", "Marzo", "Abril", "Mayo", "Junio","Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+
+val1 = window.prompt ("Introduce el numero Mes");
+console.log (Meses[val1-1]);
+document.writeln (Meses [val1-1]);
+</script>
+
+</body>
