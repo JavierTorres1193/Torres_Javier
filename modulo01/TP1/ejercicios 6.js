458095639cf856e552df197cea4083e025b9734b
JavierTorres1193 <113069095+JavierTorres1193@users.noreply.github.com>
2022-09-29 16:11:46 -0300

    Create ejercicios 6.js

---
+++ modulo01/TP1/ejercicios 6.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<h1>Dias De la semana</h1>
+</head>
+<body>
+<script>
+var days = ["lunes <br>", "martes <br>", "miercoles <br>", "Jueves <br>", "Viernes <br>", "Sabado <br>","Domingo"];
+
+for(var i = 1; i<= days.length;i++){
+}
+console.log (days);
+document.writeln (days);
+
+</script>
+
+</body>
