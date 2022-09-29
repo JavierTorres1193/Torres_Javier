81b87b3ba78dbb68faf088d97f5b2db898771ed2
JavierTorres1193 <113069095+JavierTorres1193@users.noreply.github.com>
2022-09-29 16:18:51 -0300

    Create ejercicio 4.js

---
+++ modulo01/TP2/ejercicio 4.js
@@ -0,0 +1,14 @@
+<script>
+  
+  let numero;
+numero = prompt ("Introduzca un número");
+if (numero % 2 == 0 ){
+    console.log ("El número "+ numero + " es Par")
+    document.writeln("El número "+ numero + " es Par")
+} else {
+    console.log ("El número "+ numero + " es Impar")
+    document.writeln("El número "+ numero + " es Impar")
+}
+
+
+    </script>
