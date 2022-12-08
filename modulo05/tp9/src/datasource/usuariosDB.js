879fe067f341ccfb6ecdbc66214cb5601be97b21
JavierTorres1193 <113069095+JavierTorres1193@users.noreply.github.com>
2022-12-08 16:58:40 -0300

    Create usuariosDB.js

---
+++ modulo05/tp9/src/datasource/usuariosDB.js
@@ -0,0 +1,90 @@
+const mysql = require('mysql');
+const config = require("../config/config.json");
+
+//conectarnos a nuestra DB
+var connection = mysql.createConnection(config.database);
+
+connection.connect((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("DB Conectada correctamente");
+    }
+});
+
+//fin de conexion db
+
+var usuariosDb = {};
+
+usuariosDb.getAll = function (funCallback) {
+    connection.query("SELECT * FROM usuarios where estado >=1", function (err, result, fields) {
+        if (err) {
+            funCallback({
+                message: "Surgio un problema, contactese con un administrador. Gracias",
+                detail: err
+            });
+            console.error(err);
+        } else {
+            funCallback(undefined, result);
+        }
+    });
+}
+
+
+
+usuariosDb.create = function (usuarios, funCallback) {
+    var query = 'INSERT INTO usuarios (idusuario,nickname,email,password) VALUES (?,?,?,?)'
+    var dbParams = [usuarios.idusuario, usuarios.nickname, usuarios.email, usuarios.password];
+    connection.query(query, dbParams, function (err, result, fields) {
+        if (err) {
+            if(err.code == 'ER_DUP_ENTRY'){
+                funCallback({
+                    message: `Ya existe el usuario ${usuarios.nickname}`,
+                    detail: err
+                });
+            }else{
+                funCallback({
+                    message: "Surgio un problema, contactese con un administrador. Gracias",
+                    detail: err
+                });
+            }
+            
+            console.error(err);
+        } else {
+            funCallback(undefined, {
+                message: `Se creo el usuario ${usuarios.nickname}`,
+                detail: result
+            });
+        }
+    });
+}
+
+usuariosDb.logdelete = function (idusuario, funCallback) {
+    connection.query("UPDATE usuarios SET estado = 0 WHERE idusuario = ?",idusuario, function (err, result, fields) {
+        if (err) {
+            funCallback({
+                code:3,
+                message: "Surgio un problema, contactese con un administrador. Gracias",
+                detail: err
+            }); 
+            console.error(err);
+        } else {
+            if (result.affectedRows == 0) {
+                funCallback({
+                    code:2,
+                    message: `No se encontro el id  ${idusuario} del usuario`,
+                    detail: result
+                }); 
+            } else {
+         //       console.error(err);
+                    funCallback({
+                    code:1,
+                    message: `Se modifico el usuario con el id ${idusuario}`,
+                    detail: result
+                }); 
+            }
+        }
+    });
+}
+
+module.exports = usuariosDb;
